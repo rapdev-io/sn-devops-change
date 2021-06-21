be7c56c01ded011d6cd3a8709fa1815891b29b8d
thorntonmc <mcthornton5@gmail.com>
2021-06-21 09:58:44 -0400

    init commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+(async function main() {
+	const instanceName = core.getInput('instance-name', { required: true });
+	const toolId = core.getInput('tool-id', { required: true });
+	const username = core.getInput('devops-integration-user-name', { required: true });
+	const pass = core.getInput('devops-integration-user-pass', { required: true });
+	const defaultHeaders = { 'Content-Type': 'application/json' };
+
+	const sncChangeUrl = `https://${username}:${pass}@${instanceName}.service-now.com/api/sn_devops/devops/orchestration/changeControl?toolId=${toolId}&toolType=github`;
+
+	const callbackUrl = core.getInput('callback-url', { required: true });
+
+	let githubContext = core.getInput('context-github', { required: true })
+
+	try {
+	    githubContext = JSON.parse(githubContext);
+	} catch (e) {
+	    core.setFailed(`exception parsing github context ${e}`);
+	}
+
+	let changeBody = {
+		'callbackURL': callbackUrl,
+		'orchestrationTaskURL': `${githubContext.event.repository.html_url}/actions/runs/${githubContext.run_id}rapdev-package-${githubContext.run_number}`,
+	}
+
+	let changePayload;
+	try {
+		changePayload = await axios.post(sncChangeUrl, changeBody, defaultHeaders);
+	} catch (e) {
+		changeBody = JSON.stringify(changeBody);
+		core.setFailed(`failed to create artifact package ${e} \nPayload is ${changeBody}`)
+		return
+	}
+
+})();
